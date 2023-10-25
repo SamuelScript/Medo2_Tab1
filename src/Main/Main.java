aec7770c5a62a0d7232f0987dbb82da980b63681
Samuel <samuelbernabefarias@gmail.com>
2023-10-25 18:17:33 -0300

    Correções de estabilidade nos métodos.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,13 +8,13 @@ public class Main {
         //Trab1 trab = new Trab1(20.0, 2, 0.1, 0.001, 60, 30, 1, 0, 2, 200, 10, method);
         //Trab1 trab = new Trab1(20.0, 2, 0.1, 0.1, 120, 60, 1, 0, 2, 1000, 10, method);
         try {
-            Method mtrab1 = Trab1.class.getDeclaredMethod("mtrab1", int.class);
-            Method ftbs = Trab1.class.getDeclaredMethod("FTBS", int.class);
-            Method laxfr = Trab1.class.getDeclaredMethod("Lax_Friedrichs", int.class);
-            Method laxwe = Trab1.class.getDeclaredMethod("Lax_Wendroff", int.class);
-            Method beamw = Trab1.class.getDeclaredMethod("Beam_Warming", int.class);
-            Trab1 trab = new Trab1(5, 0.5, 0.1, 0.001, 60, 30, 5, 2, 10, 1000, 5, laxwe);
+            final Method trab1 = Trab1.class.getDeclaredMethod("Algoritmo_Trabalho_1", int.class);
+            final Method ftbs = Trab1.class.getDeclaredMethod("FTBS", int.class);
+            final Method laxfr = Trab1.class.getDeclaredMethod("Lax_Friedrichs", int.class);
+            final Method laxwe = Trab1.class.getDeclaredMethod("Lax_Wendroff", int.class);
+            final Method beamw = Trab1.class.getDeclaredMethod("Beam_Warming", int.class);
 
+            Trab1 trab = new Trab1(length_x, length_xf, u, a, t_max, t_int, Ca, Cb, Cc, s_partition, draw, beamw);
             trab.run();
         } catch(Exception e) {e.printStackTrace();}
     }
